09e7767f9da1cde87425c528e26277bbaee5a632
Brandon Canady <64870171+BxCanady@users.noreply.github.com>
2025-09-24 05:02:50 -0400

    hh

--- Sparta-trucking-site/nuxt.config.ts
+++ Sparta-trucking-site/nuxt.config.ts
@@ -1,6 +1,8 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+
+
 export default defineNuxtConfig({
-  ssr: true,  // enable server-side rendering
-  nitro: {
-    preset: 'vercel'
-  }
+  compatibilityDate: '2025-07-15',
+  devtools: { enabled: true },
+
 })
