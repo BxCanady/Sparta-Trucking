05ffdb2086fa9671237967f50951b8781de4132d
Brandon Canady <64870171+BxCanady@users.noreply.github.com>
2025-09-24 04:21:01 -0400

    hh

--- Sparta-trucking-site/nuxt.config.ts
+++ Sparta-trucking-site/nuxt.config.ts
@@ -1,6 +1,14 @@
 export default defineNuxtConfig({
-  ssr: true,
+  hooks: {
+    'vite:extendConfig'(config, { isClient }) {
+      if (isClient) {
+        if (!config.resolve) config.resolve = {};
+        if (!config.resolve.alias) config.resolve.alias = {};
+        Object.assign(config.resolve.alias, { vue: 'vue/dist/vue.esm-bundler.js' });
+      }
+    }
+  },
   nitro: {
     preset: 'vercel'
-  }
+  },
 })
